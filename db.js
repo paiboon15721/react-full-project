de107925ac489938606ef010747e7dd6ba19d7e1
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-01 14:37:40 +0700

    update

--- db.js
+++ db.js
@@ -1,5 +1,26 @@
-module.exports = () => ({
-  posts: [{ id: 1, title: 'json-server', author: 'typicode' }],
-  comments: [{ id: 1, body: 'some comment', postId: 1 }],
-  profile: { name: 'typicode' },
-})
+const faker = require('faker')
+
+const data = {
+  users: gen(
+    {
+      id: '{{random.number}}',
+      name: '{{company.companyName}} {{company.companySuffix}}',
+      address: '{{address.streetAddress}}',
+      phone: '{{phone.phoneNumber}}',
+      email: '{{internet.email}}',
+    },
+    100,
+  ),
+}
+
+function gen(schema, min = 1, max) {
+  max = max || min
+  return Array.from({ length: faker.random.number({ min, max }) }).map(() =>
+    Object.keys(schema).reduce((entity, key) => {
+      entity[key] = faker.fake(schema[key])
+      return entity
+    }, {}),
+  )
+}
+
+module.exports = () => data
