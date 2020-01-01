f33ef3b0415d7de6030124c6f789fd218ba04880
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-01 16:07:47 +0700

    update

--- db.js
+++ db.js
@@ -1,23 +1,27 @@
 const faker = require('faker')
 
+const departments = ['staff', 'manager', 'sale', 'account', 'specialist']
+
 const data = {
-  users: gen(
+  employees: gen(
     {
-      id: '{{random.number}}',
-      name: '{{company.companyName}} {{company.companySuffix}}',
-      address: '{{address.streetAddress}}',
-      phone: '{{phone.phoneNumber}}',
-      email: '{{internet.email}}',
+      id: faker.random.number,
+      name: faker.name.firstName,
+      dob: () => faker.date.between('1980-01-01', '2000-12-31'),
+      salary: () => faker.finance.amount(25000, 80000),
+      department: () =>
+        departments[faker.random.number(departments.length - 1)],
+      phone: faker.phone.phoneNumber,
+      email: faker.internet.email,
     },
     100,
   ),
 }
 
-function gen(schema, min = 1, max) {
-  max = max || min
-  return Array.from({ length: faker.random.number({ min, max }) }).map(() =>
+function gen(schema, length) {
+  return Array.from({ length }).map(() =>
     Object.keys(schema).reduce((entity, key) => {
-      entity[key] = faker.fake(schema[key])
+      entity[key] = schema[key]()
       return entity
     }, {}),
   )
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Button } from 'antd'
+import 'antd/dist/antd.css'
 
-const App = () => <h1>Hello</h1>
+const App = () => <Button>Add</Button>
 
 export default App
