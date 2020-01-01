2798402e46c7e637c85029ab4ceca043b2728443
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-01 17:00:10 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React from 'react'
 import Layout from 'components/Layout'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import Dashboard from 'pages/Dashboard'
+import Employee from 'pages/Employee'
 
-const App = () => <Layout>Hello</Layout>
+const App = () => (
+  <Router>
+    <Layout>
+      <Switch>
+        <Route path="/employee">
+          <Employee />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <Redirect to="/dashboard" />
+        </Route>
+      </Switch>
+    </Layout>
+  </Router>
+)
 
 export default App
