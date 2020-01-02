94b55ef5417b8eae3f3137e8235d993be7e1c2fe
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-01-02 14:22:49 +0700

    update

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,29 @@
 import { useState, useEffect } from 'react'
+import { CancelToken, isCancel } from 'axios'
 import axios from 'utils/axios'
 
 export default url => {
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
 
-  const fetchData = async () => {
-    setLoading(true)
-    const { data } = await axios.get(url)
-    setData(data.map(v => ({ ...v, key: v.id })))
-    setLoading(false)
-  }
-
   useEffect(() => {
+    const source = CancelToken.source()
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const { data } = await axios.get(url, { cancelToken: source.token })
+        setData(data.map(v => ({ ...v, key: v.id })))
+        setLoading(false)
+      } catch (e) {
+        if (isCancel(e)) {
+          console.log('Request canceled', e.message)
+        }
+      }
+    }
     fetchData()
+    return () => {
+      source.cancel('Operation canceled')
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
